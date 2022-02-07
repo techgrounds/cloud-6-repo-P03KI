7871c80b48e96745513fe7beec54f8c721d7b668
Suheri <31050194+P03KI@users.noreply.github.com>
2022-02-07 13:03:39 +0100

    ..

--- Python/Scripts/02Variables2.py
+++ Python/Scripts/02Variables2.py
@@ -1,6 +1,3 @@
-from smtplib import SMTPAuthenticationError
-
-
 name = "suheri"
 
 print("Hello, " + name + "!")
--- Python/Scripts/03Datatypes1.py
+++ Python/Scripts/03Datatypes1.py
@@ -1,8 +1,10 @@
 # initial vars
+
 a = 'int'
 b = 7
 c = False
 d = "18.5"
 
+# print vartypes met type()
 print("a=" + type(a) + " b=" + type(b) + " c=" + type(c) + " d=" + type(d))
 
