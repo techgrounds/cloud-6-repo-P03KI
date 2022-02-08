2ffa11b2b63c4c3fc2c00c7a2f35f2fd04d0c899
P03KI <suheri@gmail.com>
2022-02-08 16:30:28 +0100

    upd 15.py

--- Python/PLS FIX/15.py
+++ Python/PLS FIX/15.py
@@ -3,12 +3,13 @@ The output should be:
 a5|||5|||5|||a5|||5|||5|||a5|||5|||5|||
 '''
 foo = ''
-for i in range(9):
-    if i == 0 or 3 or 6:
+for i in range(12):
+    if i == 0 or i == 4 or i == 8:
 	    foo += 'a'
-	for j in range(1):
-		foo += '5'
-	for k in range(3):
-		foo += '|'
+    else:
+	    for j in range(1):
+		    foo += '5'
+	    for k in range(3):
+		    foo += '|'
 
 print(foo)
